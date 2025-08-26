e8369fc7185bb0c27780d63a66fc6d02eb06efcd
Sean <robertelston@lcad.edu>
2025-08-26 12:11:13 -0700

    added rotation too the cube, via updated script

--- Assets/script_01.cs
+++ Assets/script_01.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class script_01 : MonoBehaviour
 {
+    public float rotationspeed = 30f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class script_01 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, rotationspeed * Time.deltaTime, 0);
     }
 }
